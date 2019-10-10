1c4fc9383ac567e67e4fba0ba06c618603d4d08b
cpfcz@126.com <cpfcz@126.com>
2019-10-10 15:16:16 +0800

    update tracking fileslist

--- launch_vrep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash  
-echo "Launching Stuff"  
-roscore &
-echo "Core started" 
-cd V-REP
-./vrep.sh scenes/Ginger.ttt  
-wait 
-cd ..
-ps -A |grep roscore |grep -v grep|cut -c 9-15|xargs kill -1  
